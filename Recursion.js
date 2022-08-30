921631d73e98632d2844ae8a2c6dc5c317534823
Kamran <kamraneg@gmail.com>
2022-08-31 00:39:08 +0530

    range using recursion

--- Recursion.js
+++ Recursion.js
@@ -39,16 +39,34 @@ function factorial(x) {
     }
 }
 
-// const num = -1;
-
-console.log(factorial(10))
+let num =5
+// console.log(factorial(10))
 
 // calling factorial() if num is non-negative
-// if (num > 0) {
-//     let result = factorial(num);
-//     console.log(`The factorial of ${num} is ${result}`);
-// }
+if (num > 0) {
+    let result = factorial(num);
+    console.log(`The factorial of ${num} is ${result}`);
+}
 
 
 
+// Write a JavaScript program to get the integers in range (x, y) using recursion.
+// Input: range(1, 5)
+// Output:[1, 2, 3, 4, 5]
+// Explanation: All the number between 1 and 5.
+// Constraints:Restrict to the input format that is mentioned.
 
+function range(start, end){
+    let li =[];
+    recurse(start,end+1,li)
+    console.log(li)
+}
+
+function recurse(start, end, li){
+    if(end==start)
+        return start;
+    let push =recurse(start,end-1,li)
+    li.push(push)
+    return end;
+}
+  console.log(range(1,5))
