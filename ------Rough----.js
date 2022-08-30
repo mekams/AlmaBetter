b998559001a933ea8b663c80d66b2c04427a67a9
Kamran <kamraneg@gmail.com>
2022-08-31 00:00:48 +0530

    Rough code---commented all

--- ------Rough----.js
+++ ------Rough----.js
@@ -412,59 +412,59 @@ const { indexOf } = require("lodash")
 
 
 // program to implement stack data structure
-class Stack {
-    constructor() {
-        this.items = [];
-    }
+// class Stack {
+//     constructor() {
+//         this.items = [];
+//     }
     
-    // add element to the stack
-    add(element) {
-        return this.items.push(element);
-    }
+//     // add element to the stack
+//     add(element) {
+//         return this.items.push(element);
+//     }
     
-    // remove element from the stack
-    remove() {
-        if(this.items.length > 0) {
-            return this.items.pop();
-        }
-    }
+//     // remove element from the stack
+//     remove() {
+//         if(this.items.length > 0) {
+//             return this.items.pop();
+//         }
+//     }
     
-    // view the last element
-    peek() {
-        return this.items[this.items.length - 1];
-    }
+//     // view the last element
+//     peek() {
+//         return this.items[this.items.length - 1];
+//     }
     
-    // check if the stack is empty
-    isEmpty(){
-       return this.items.length == 0;
-    }
+//     // check if the stack is empty
+//     isEmpty(){
+//        return this.items.length == 0;
+//     }
    
-    // the size of the stack
-    size(){
-        return this.items.length;
-    }
+//     // the size of the stack
+//     size(){
+//         return this.items.length;
+//     }
  
-    // empty the stack
-    clear(){
-        this.items = [];
-    }
-}
+//     // empty the stack
+//     clear(){
+//         this.items = [];
+//     }
+// }
 
-let stack = new Stack();
-stack.add(1);
-stack.add(2);
-stack.add(4);
-stack.add(8);
-console.log(stack.items);
+// let stack = new Stack();
+// stack.add(1);
+// stack.add(2);
+// stack.add(4);
+// stack.add(8);
+// console.log(stack.items);
 
-stack.remove();
-console.log(stack.items);
+// stack.remove();
+// console.log(stack.items);
 
-console.log(stack.peek());
+// console.log(stack.peek());
 
-console.log(stack.isEmpty());
+// console.log(stack.isEmpty());
 
-console.log(stack.size());
+// console.log(stack.size());
 
-stack.clear();
-console.log(stack.items);
+// stack.clear();
+// console.log(stack.items);
