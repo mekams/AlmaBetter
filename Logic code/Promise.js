898d929b51369c95371995453324c404453002fa
Kamran Mustafa <kamraneg@gmail.com>
2023-01-23 16:15:36 +0530

    Update Promise.js

--- Logic code/Promise.js
+++ Logic code/Promise.js
@@ -1,10 +1,29 @@
-let promise = new Promise(function(resolve, reject) {
-    reject('Promise')
-    })
+// let promise = new Promise(function(resolve, reject) {
+//     reject('Promise')
+//     })
     
-    promise.then(function(successMessage) {
-    console.log('Resolved '+successMessage);
-    }, function(errorMessage) {
-    //error handler function is invoked
-    console.log('Rejected '+errorMessage)
+//     promise.then(function(successMessage) {
+//     console.log('Resolved '+successMessage);
+//     }, function(errorMessage) {
+//     //error handler function is invoked
+//     console.log('Rejected '+errorMessage)
+//     })
+
+
+let promise = new Promise((resolve,reject)=>{           //predefined syntax
+    console.log("promise is pending")
+    setTimeout(()=>{
+        reject("promise")                             //promise will be  rejected /resolved          
+    },4000)
     })
+
+    promise.then((value)=>{
+        console.log("Resolved :",value)                //success
+    },((error)=>{
+        console.log(`Rejected :${error}`)               //catch error
+    }))
+
+//if sure error will come then line 27 to 29
+    // promise.catch((error)=>{                            //specifically to catch errors
+    //     console.log("Reject",error)                    // line 20 to 24 must be written if any doubt of errors
+    // })
